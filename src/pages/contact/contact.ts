edb0a4cb5cdead5827a7f8d5759720d482989be3
hujun <azhujun_githun@163.com>
2017-02-19 21:37:00 +0800

    更新宝藏模块

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  @ViewChild('mySlider') slider:Slides;
+  mySlideOptions={
+    autoplay:0,
+    initialSlide:0,
+    pager:false,
+    loop:true,
+    speed:300
+  };
   constructor(public navCtrl: NavController) {
 
   }
+
 
 }
