6a28ada0e34550fa8fc3f9510c340a7623c78072
hujun <azhujun_githun@163.com>
2017-03-07 11:04:59 +0800

    购物车选择

--- src/pages/goodsinfo/goodsinfo.ts
+++ src/pages/goodsinfo/goodsinfo.ts
@@ -12,11 +12,78 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'goodsinfo.html'
 })
 export class GoodsinfoPage {
-
+  public Goods = null;
+  public isActiveGoods = null;
+  public GoodsCAR = [];
+  public number = 1;
+  public GoodsIndex = null;
+  public GoodsIndexSize = null;
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GoodsinfoPage');
+    this.Goods = [
+      {
+        name:'妮子大衣',
+        color:'红褐色',
+        size:{
+          s:200,
+          m:23,
+          l:46,
+          xl:55
+        }
+      },
+      {
+        name:'妮子大衣',
+        color:'红色',
+        size:{
+          s:200,
+          m:23,
+          l:46,
+          xl:55
+        }
+      }
+    ]
   }
 
+  plus(){
+    if(this.number < this.GoodsIndex.size[this.GoodsIndexSize]){
+      this.number++;
+    }else{
+      console.log('没这么多')
+    }
+  }
+  jian(){
+    if(this.number>1){
+      this.number--;
+    }
+  }
+
+  ACTIVE_GOODS(item){
+    this.GoodsIndex = item;
+    let ActiveArray = [];
+    for (var key in item.size) {
+      if (item.size.hasOwnProperty(key)) {
+        ActiveArray.push(key)
+      }
+    }
+    this.isActiveGoods=ActiveArray;
+  }
+
+  addCar(){
+    this.GoodsCAR.push({
+      goodsName: this.GoodsIndex.name,
+      goodsColor: this.GoodsIndex.color,
+      goodsNumber: this.number,
+      goodsSize:this.GoodsIndexSize
+    })
+    console.log(this.GoodsCAR)
+  }
+  ACTIVE_GOODS_SIZE(size){
+    this.GoodsIndexSize = size;
+    this.number = 1;
+  }
+  CanOrder(){
+    console.log(1)
+  }
 }
